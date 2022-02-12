66b5aa203acb1d33c86f4844f9c679051557f5f9
joshua ayaviri <93785825+ayaviri@users.noreply.github.com>
2022-02-12 13:05:58 -0500

    Initial setup (#1)
    * Creation of React App + Hello World
    
    * Added PR template

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world. Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <br />
+      </header>
+    </div>
+  );
+}
+
+export default App;
