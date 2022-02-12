dae191945c5c013f18e66a939320177695ab296b
Will WS <willwallsoto@gmail.com>
2022-02-12 16:34:07 -0500

    get reddit posts

--- backend/api.js
+++ backend/api.js
@@ -1,19 +1,40 @@
 import axios from 'axios';
 import * as secrets from './secrets.js';
 
-export const getTumblrData = async (searchQuery) => {
-  try {
-    const apiResponse = await axios.get(`https://api.tumblr.com/v2/tagged?tag=${searchQuery}&api_key=${secrets.TUMBLR_API_KEY}&limit=1`, {
-      headers: {
-        'Content-type': 'application/json'
-      }
-    });
-    
-    // logs to the url to the top post for this search query
-    console.log(apiResponse.data.response[0].post_url);
-  } catch (error) {
-    console.log(error.message);
-  }
+export const getTumblrData = async(searchQuery) => {
+    try {
+        const apiResponse = await axios.get(`https://api.tumblr.com/v2/tagged?tag=${searchQuery}&api_key=${secrets.TUMBLR_API_KEY}&limit=1`, {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+        // logs to the url to the top post for this search query
+        console.log(apiResponse.data.response[0].post_url);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
+
+export const getRedditPosts = async(searchQuery) => {
+    try {
+        const apiResponse = await axios.get(`https://www.reddit.com/search.json?q=${searchQuery}&limit=5`, {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+        // logs to the url to the top post for this search query
+        console.log(apiResponse.data.data.children.map((x) => {
+            return {
+                "title": x.data.title,
+                "img": x.data.url,
+                "link": `https://www.reddit.com/${x.data.permalink}`
+            }
+        }));
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 getTumblrData('fnaf');
