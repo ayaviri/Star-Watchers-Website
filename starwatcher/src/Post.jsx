95492c1421b3fe069e6fd46af53f5d57a896d82d
Will WS <willwallsoto@gmail.com>
2022-02-13 03:37:50 -0500

    prevent empty posts

--- starwatcher/src/Post.jsx
+++ starwatcher/src/Post.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Post extends Component {
     render() { 
         return (<div>
-            <h5>{this.props.name}</h5>
+            <h5>{this.props.name?.length > 0 ? this.props.name : "Untitled Post"}</h5>
             <a className="btn btn-primary" href={this.props.link}>See On Site</a>
         </div>);
     }
