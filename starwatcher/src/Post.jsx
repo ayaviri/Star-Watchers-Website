090c2c58e290de536c4ddd7dc4f3b3cee3ec2d9f
bzeng68 <92894959+bzeng68@users.noreply.github.com>
2022-02-13 02:50:47 -0500

    site look better (#4)

--- starwatcher/src/Post.jsx
+++ starwatcher/src/Post.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { FaPaperclip } from "react-icons/fa";
+import { IconContext } from 'react-icons/lib';
 
 class Post extends Component {
     render() { 
         return (<div>
-            <h5>{this.props.name}</h5>
-            <a className="btn btn-primary" href={this.props.link}>See On Site</a>
+            <IconContext.Provider value ={{ color: "cornflowerblue"}}>
+            <ListGroup.Item>
+            <h5><a href={this.props.link}><FaPaperclip/></a> {this.props.name}</h5>
+            </ListGroup.Item>
+            </IconContext.Provider>
         </div>);
     }
 }
